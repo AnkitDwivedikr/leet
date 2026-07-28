215b6b23119ebb3d3bcfad0059f29e66feac0f3e
Ankit Kumar Dwivedi <ankitdwivedikr@gmail.com>
2026-07-28 15:00:52 +0530

    Time: 24 ms (80.00%) | Memory: 73.7 MB (44.61%) - LeetSync

---
+++ 3812-smallest-palindromic-rearrangement-i/smallest-palindromic-rearrangement-i.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    string smallestPalindrome(string s) {
+
+        vector<int> cnt(26, 0);
+
+        for (char c : s) {
+            cnt[c - 'a']++;
+        }
+
+        string left = "";
+        char mid = '\0';
+
+        for (int i = 0; i < 26; i++) {
+
+            left.append(cnt[i] / 2, char(i + 'a'));
+
+            if (cnt[i] % 2 == 1)
+                mid = char(i + 'a');
+        }
+
+        string right = left;
+        reverse(right.begin(), right.end());
+
+        if (mid != '\0')
+            return left + mid + right;
+
+        return left + right;
+    }
+};//akd
